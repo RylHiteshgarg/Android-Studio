bdc0d37fdeb86e2ce03d7ae91f725d5ecc58ecf6
RylHiteshgarg <43132149+RylHiteshgarg@users.noreply.github.com>
2020-05-21 14:55:15 +0530

    Add files via upload

---
+++ Pet Adoption/generics/src/main/java/com/delta/generics/Adoptable.java
@@ -0,0 +1,14 @@
+package com.delta.generics;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by hitesh on 5/15/20.
+ */
+public interface Adoptable {
+
+    public int getRating();
+    public String getNumber();
+    public boolean isAvailable = false;
+
+}
---
+++ Pet Adoption/generics/src/main/java/com/delta/generics/GenericsActivity.java
@@ -0,0 +1,101 @@
+package com.delta.generics;
+
+import android.app.Activity;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+import android.service.dreams.DreamService;
+import android.util.StringBuilderPrinter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsActivity extends Activity {
+
+    public TextView nameTextView = null;
+    public TextView descriptionTextView = null;
+    public RatingBar ratingView = null;
+    public ImageView portraitView = null;
+    public Button nextButton = null;
+
+    private int currentSelection = 0;
+
+    CatAdapter catAdapter;
+
+    AdoptAdapter<Cat>catAdoptAdapter;
+    AdoptAdapter<Dog>dogAdoptAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generics);
+
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        descriptionTextView = (TextView) findViewById(R.id.descriptionTextView);
+        ratingView = (RatingBar) findViewById(R.id.ratingView);
+        portraitView = (ImageView) findViewById(R.id.portraitView);
+        nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNext();
+            }
+        });
+
+        catAdapter = new CatAdapter(this,nameTextView,descriptionTextView,ratingView,portraitView);
+        catAdapter.set(AdoptData.mCatList.get(0));
+
+        catAdoptAdapter = new AdoptAdapter<Cat>(this,nameTextView,descriptionTextView,ratingView,portraitView);
+        dogAdoptAdapter = new AdoptAdapter<Dog>(this,nameTextView,descriptionTextView,ratingView,portraitView);
+
+        catAdoptAdapter.set(AdoptData.mCatList.get(0));
+        dogAdoptAdapter.set(AdoptData.mDogList.get(0));
+    }
+
+    public void showNext(){
+        int random = currentSelection;
+        while(random == currentSelection){
+            //avoid same selection twice.
+            random = (int )(Math.random() * AdoptData.mCatList.size());
+            random = (int )(Math.random() * AdoptData.mDogList.size());
+        }
+        currentSelection = random;
+        Cat c = AdoptData.mCatList.get(random);
+        Dog d = AdoptData.mDogList.get(random);
+
+        catAdoptAdapter.set(c);
+        dogAdoptAdapter.set(d);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.generics, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
